8793086c0d37056d04c7f77a5a08b3f4df57f5db
NaveenKumar124 <57155848+NaveenKumar124@users.noreply.github.com>
2023-05-04 00:32:27 -0400

    Add files via upload

---
+++ Tipsy-iOS13/Tipsy/Models/TIP.swift
@@ -0,0 +1,9 @@
+//
+//  TIP.swift
+//  Tipsy
+//
+//  Created by Navi Malhotra on 2023-05-03.
+//  Copyright © 2023 The App Brewery. All rights reserved.
+//
+
+import Foundation
---
+++ Tipsy-iOS13/Tipsy/Models/TipsyBrain.swift
@@ -0,0 +1,9 @@
+//
+//  TipsyBrain.swift
+//  Tipsy
+//
+//  Created by Navi Malhotra on 2023-05-03.
+//  Copyright © 2023 The App Brewery. All rights reserved.
+//
+
+import Foundation
